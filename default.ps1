73b41072dd25252621b6560690ac00c18a5b4a54
Christian Krowiorsch <christian.krowiorsch@googlemail.com>
2015-11-05 17:47:45 +0100

    initial setup

---
+++ default.ps1
@@ -0,0 +1,60 @@
+Framework('4.5.1')
+$erroractionpreference = "Stop"
+
+properties {
+    $location = (get-location);
+    $outdir = (join-path $location "Build");
+    $artifactsdir = (join-path $outdir "Artifacts");
+    $bindir = (join-path $outdir "Bin");
+    $passtestdir = (join-path $bindir "passtest");
+    $env = "";
+}
+
+task default -depends Help
+task ci -depends rebuild,test
+
+
+task Help {
+  write-host "FolgendeTargets: ci, rebuild, test" -ForegroundColor Yellow
+}
+
+task Check {
+}
+
+
+task Clean -depends NugetRestore {
+	[void](rmdir -force -recurse $outdir -ea SilentlyContinue)
+}
+
+task NugetRestore {
+  exec { .nuget\nuget.exe restore }
+}
+
+task Rebuild -depends Clean {
+  exec { .nuget\nuget.exe restore }
+
+	$solution = get-location;
+
+  exec { msbuild /nologo /v:minimal /t:rebuild /p:"Configuration=Release;OutputPath=$bindir/Lucene.Zealous/;SolutionDir=$solution/" "Source/Lucene.Zealous/Lucene.Zealous.csproj" }
+}
+
+task Test -depends Clean {
+  [void](mkdir $artifactsdir)
+
+  exec { .nuget\nuget.exe restore }
+
+  exec {.nuget\nuget install Machine.Specifications.Runner.Console -OutputDirectory Packages}
+	$mspecdir = (resolve-path ".\Packages\Machine.Specifications.Runner.Console.0.*\")
+	$mspec = @("$mspecdir\tools\mspec-x86-clr4.exe", "--xml", "$artifactsdir\mspec-results.xml", "--html", "$artifactsdir\mspec-results.html", "-x", "NeedStagingElasticSearch");
+
+	foreach($testProj in (dir -Filter ".\Source\*.Tests")){
+		exec { msbuild /nologo /v:m /t:rebuild /p:"Configuration=Release;OutputPath=$bindir/$testProj" "Source/$testProj/$testProj.csproj" }
+		$mspec += "$bindir\$testProj\$testProj.dll";
+	}
+
+	try {exec { &([scriptblock]::create($mspec -join ' ')) }} catch {}
+
+	$xslt = New-Object System.Xml.Xsl.XslCompiledTransform;
+  $xslt.Load("Packages\MSpecToJUnit\MSpecToJUnit.xlt");
+  $xslt.Transform("$artifactsdir\mspec-results.xml", "$artifactsdir\junit-results.xml");
+}
