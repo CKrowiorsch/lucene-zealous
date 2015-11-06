e70f8c101844a52e29641cf4c49ffa5d75edd368
Christian Krowiorsch <christian.krowiorsch@googlemail.com>
2015-11-06 13:40:54 +0100

    add missing files

---
+++ Source/Lucene.Zealous.Tests/Exentsions/TokenFilterExtentions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Tokenattributes;
+using Token = Krowiorsch.Lucene.Model.Token;
+
+namespace Krowiorsch.Lucene.Exentsions
+{
+    static class TokenFilterExtentions
+    {
+        internal static IEnumerable<Token> ReadTokens(this TokenFilter filter)
+        {
+            var termAttribute = filter.GetAttribute<ITermAttribute>();
+
+            var positionAttribute = filter.HasAttribute<IPositionIncrementAttribute>()
+                ? filter.GetAttribute<IPositionIncrementAttribute>()
+                : null;
+
+            var offsetAttribute = filter.GetAttribute<IOffsetAttribute>();
+
+            while(filter.IncrementToken())
+            {
+                yield return new Token
+                {
+                    Term = termAttribute.Term,
+                    PositionIncrement = positionAttribute != null ? positionAttribute.PositionIncrement : 0,
+                    StartOffset = offsetAttribute.StartOffset,
+                    EndOffset = offsetAttribute.EndOffset
+                };
+            }
+        } 
+    }
+}
---
+++ Source/Lucene.Zealous.Tests/Model/Token.cs
@@ -0,0 +1,11 @@
+﻿namespace Krowiorsch.Lucene.Model
+{
+    /// <summary> only for tetsing purpose </summary>
+    internal class Token
+    {
+        public string Term { get; set; } 
+        public int PositionIncrement { get; set; }
+        public int StartOffset { get; set; }
+        public int EndOffset { get; set; }
+    }
+}
