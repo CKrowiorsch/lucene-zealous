8e767698fc5c4d2dbea0483e0de7064914bc48ca
Christian Krowiorsch <christian.krowiorsch@googlemail.com>
2015-11-06 16:05:28 +0100

    add flag filter

---
+++ Source/Lucene.Zealous/Filter/FlagFilter.cs
@@ -0,0 +1,33 @@
+﻿using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Tokenattributes;
+
+namespace Krowiorsch.Lucene.Filter
+{
+    /// <summary> filters filters every token that have a specific flag</summary>
+    public sealed class FlagFilter : TokenFilter
+    {
+        readonly IFlagsAttribute _flagsAttribute;
+        readonly int _flags;
+
+        public FlagFilter(int flags, TokenStream input)
+            : base(input)
+        {
+            _flagsAttribute = AddAttribute<IFlagsAttribute>();
+            _flags = flags;
+        }
+
+        public override bool IncrementToken()
+        {
+            while (input.IncrementToken())
+            {
+                // when flag is set -> discard
+                if ((_flagsAttribute.Flags & _flags) == _flags)
+                    continue;
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
