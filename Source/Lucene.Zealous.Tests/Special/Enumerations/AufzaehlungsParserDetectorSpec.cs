14ade978a4caab75672819150f49a789b70f8e9c
Christian Krowiorsch <krowiorsch@landaumedia.de>
2016-04-26 08:40:38 +0200

    add base tests for enumerations

---
+++ Source/Lucene.Zealous.Tests/Special/Enumerations/AufzaehlungsParserDetectorSpec.cs
@@ -0,0 +1,14 @@
+﻿using LandauMedia.CommandCenter.Infrastructure.TextSearchAlgorithm.Analyzers.Filters.Aufzaehlung;
+
+namespace Krowiorsch.Lucene.Special.Enumerations
+{
+    class with_Aufzaehlungsparser
+    {
+        protected static void SetupFilter(string text)
+        {
+            _sut = new AufzaehlungDetector();
+        }
+
+        protected static AufzaehlungDetector _sut;
+    }
+}
